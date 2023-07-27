2c86480dd69f07bea54247f66f58537dedaf7d89
Hammad-Asif-Official <034928hammadasif582@gmail.com>
2023-07-27 11:54:42 +0600

    ch1-commit

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const lib = require('./module.js')
 
+const fs = require('fs');
+// const txt = fs.readFileSync('demo.txt', "utf-8");
+// console.log(txt);
+
+fs.readFile('demo.txt', "utf-8", (err, txt) => {
+    console.log(txt);
+
+})
+
+
 console.log(lib.sum(2, 3), lib.mul(3, 4))
