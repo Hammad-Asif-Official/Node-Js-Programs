b9f2a8599b1e3a195b67a796970b7865aecf4350
Hammad-Asif-Official <034928hammadasif582@gmail.com>
2023-08-01 13:02:15 +0600

    MVC change

--- controller/user.js
+++ controller/user.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
 const users = data.users;
 
+
 exports.createUser = (req, res) => {
     console.log(req.body);
     users.push(req.body);
